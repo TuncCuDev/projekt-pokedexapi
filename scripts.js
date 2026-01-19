4f1783c082c1cb7f7c8bffd97fa6cbb252909459
TuncCu <tuncay-cubukcu@hotmail.de>
2026-01-19 12:39:30 +0100

    created the main card with text

--- scripts.js
+++ scripts.js
@@ -13,20 +13,32 @@ async function loadPokemon() {
     responseToJson = await response.json();
     pokemon = responseToJson.results;
 
-    showPokemon();
-    getIdFromURL();
+    await showPokemon();
+}
+
+async function getPokemonTypes(id) {
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+    let data = await response.json();
+
+    let types = [];
+    for (let i = 0; i < data.types.length; i++){
+
+        types[i] = data.types[i].type.name;
+    } return types;
 }
 
 
-function showPokemon() {
+async function showPokemon() {
     let pokemonContent = document.getElementById("content");
     pokemonContent.innerHTML = "";
 
     for (let i = 0; i < pokemon.length; i++) {
         let singlePokemon = pokemon[i];
         let id = getIdFromURL(singlePokemon);
+        let types = await getPokemonTypes(id);
 
-    pokemonContent.innerHTML += getPokemonTemplates(singlePokemon, id);
+
+    pokemonContent.innerHTML += getPokemonTemplates(singlePokemon, id, types);
     }
 }
 
@@ -34,24 +46,30 @@ function showPokemon() {
 function getIdFromURL(singlePokemon) {
     let url = singlePokemon.url
     let seperateURL = url.split("/");
-    let id = seperateURL[seperateURL.length-2]
-    console.log(id);
+    let id = seperateURL[seperateURL.length-2];
     return id;
 }
 
 
-function getPokemonTemplates(singlePokemon, id) {
+function getPokemonTemplates(singlePokemon, id, types) {
+    let typesHTML = "";
+    for (let i = 0; i < types.length; i++) {
+        typesHTML += `<span class="pokemon-type"> ${types[i]}</span>`;
+        
+    }
     return `<main class=""main-card>  
                 <section class="card-inside">
                     <div class="card" style="width: 18rem;">
                         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" class="card-img-top" alt="...">
                         <div class="card-body">
-                        <p class="card-id"></p>
-                        <span>${id} </span>
-                        <h5 class="card-title">${singlePokemon.name}</h5>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <div class="card-id">
+                                <h4> #${id} </h4>
+                                <h5 class="card-title">${singlePokemon.name}</h5>
+                            </div>
+                            <div class="pokemon-types">${typesHTML}</div>
                         </div>
                     </div>
                 </section>
             </main>`
+                
 }
