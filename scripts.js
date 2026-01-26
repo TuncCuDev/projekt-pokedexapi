6e1284bcda6227c2f0b62fc3477edf9c4e80e19b
TuncCu <tuncay-cubukcu@hotmail.de>
2026-01-26 12:35:26 +0100

    created overlay card with image, name, id, and types of pokemon

--- scripts.js
+++ scripts.js
@@ -54,13 +54,14 @@ function getIdFromURL(singlePokemon) {
 function getPokemonTemplates(singlePokemon, id, types) {
     let typesHTML = "";
     for (let i = 0; i < types.length; i++) {
-        typesHTML += `<span class="pokemon-type"> ${types[i]}</span>`;
-        
+        typesHTML += `<span class="pokemon-type ${types[i]}">${types[i]}</span>`;
+
+       
     }
-    return `<main class=""main-card>  
-                <section class="card-inside">
+    return `<main class="main-card">  
+                <section class="card-inside" onclick="openPokemonCard(${id})" >
                     <div class="card" style="width: 18rem;">
-                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" class="card-img-top" alt="...">
+                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" class="card-img-top" alt="Pokemon-Image">
                         <div class="card-body">
                             <div class="card-id">
                                 <h4> #${id} </h4>
@@ -70,6 +71,61 @@ function getPokemonTemplates(singlePokemon, id, types) {
                         </div>
                     </div>
                 </section>
-            </main>`
-                
+            </main>`   
+}
+
+async function openPokemonCard(id) {
+    let selectedPokemon;
+
+    for (let i = 0; i < pokemon.length; i++) {
+        if (getIdFromURL(pokemon[i]) == id) {
+        selectedPokemon = pokemon[i];
+        break;
+        }    
+    }
+
+    let types = await getPokemonTypes(id);
+
+    let overlay = document.getElementById('overlay');
+    let overlayCard = document.getElementById('overlay-card');
+
+    overlay.classList.remove('d_none');
+    console.log(id, selectedPokemon.name );
+    overlayCard.innerHTML = getOverlayCard(id, selectedPokemon.name, types);
+}
+
+
+function getOverlayCard(id, name, types) {
+    let typesHTML = "";
+
+    for (let i = 0; i < types.length; i++) {
+        typesHTML += `<span class="pokemon-type overlay-pokemon-type ${types[i]}">${types[i]}</span>`;
+    }
+
+    return `<div class="main-overlay">
+            <header>
+                <div class="header-first-line">
+                    <div id="overlayPokemonId">#${id}</div>
+                    <div id="overlayPokemonName">${name}</div>
+                    <p class="overlay-close-button" onclick="closePokemonCard()"> X </p>
+                </div>
+                <div class="header-second-line">
+                    <img class="overlay-pokemon-image" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" alt="">
+                </div>
+                <div class="overlay-pokemon-types">
+                    ${typesHTML}
+                </div>
+            </header>
+
+        <main>
+            
+        </main>
+
+        </div>
+           `
+}
+
+function closePokemonCard() {
+    document.getElementById('overlay').classList.add('d_none');
+    document.getElementById('overlay-card').innerHTML = "";
 }
