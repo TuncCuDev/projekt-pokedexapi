5e506d238a135e735ef5a1bfb5b49804ebff7997
TuncCu <tuncay-cubukcu@hotmail.de>
2026-02-01 13:27:38 +0100

    created overlayscripts.js for scripts data, clean code

--- scripts/overlayscripts.js
+++ scripts/overlayscripts.js
@@ -1,125 +1,83 @@
-const Pokedex_URL = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
-
-let pokemon = [];
-let currentPokemonIndex = 0;
-currentPokemonData = 0;
-let currentTab = "main";
-
-
-function loadAndShowPokemon() {
-    loadPokemon();
-}
-
-
-async function loadPokemon() {
-    let response = await fetch(Pokedex_URL)
-    responseToJson = await response.json();
-    pokemon = responseToJson.results;
-
-    await showPokemon();
-}
-
-
-async function getPokemonDetails(id) {
-    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
-    let data = await response.json();
-    return data;
-}
-
-
-async function getPokemonTypes(id) {
-    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
-    let data = await response.json();
-
-    let types = [];
-    for (let i = 0; i < data.types.length; i++){
-        types[i] = data.types[i].type.name;
-    } return types;
-}
-
-async function getPokemonMainInfo(id) {
-     let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
-    let data = await response.json();
-
-    console.log(data)
-}
-
-
-async function showPokemon() {
-    let pokemonContent = document.getElementById("content");
-    pokemonContent.innerHTML = "";
-
-    for (let i = 0; i < pokemon.length; i++) {
-        let singlePokemon = pokemon[i];
-        let id = getIdFromURL(singlePokemon);
-        let types = await getPokemonTypes(id);
-
-
-    pokemonContent.innerHTML += getPokemonTemplates(singlePokemon, id, types);
-    }
-}
-
-
-function getIdFromURL(singlePokemon) {
-    let url = singlePokemon.url
-    let seperateURL = url.split("/");
-    let id = seperateURL[seperateURL.length-2];
-    return id;
-}
-
-
-function getPokemonTemplates(singlePokemon, id, types) {
-    let typesHTML = "";
-    for (let i = 0; i < types.length; i++) {
-        typesHTML += `<span class="pokemon-type ${types[i]}">${types[i]}</span>`;
-
-       
-    }
-    return `<main class="main-card">  
-                <section class="card-inside" onclick="openPokemonCard(${id})" >
-                    <div class="card" style="width: 18rem;">
-                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" class="card-img-top" alt="Pokemon-Image">
-                        <div class="card-body">
-                            <div class="card-id">
-                                <h4> #${id} </h4>
-                                <h5 class="card-title">${singlePokemon.name}</h5>
-                            </div>
-                            <div class="pokemon-types">${typesHTML}</div>
-                        </div>
-                    </div>
-                </section>
-            </main>`   
-}
-
-async function openPokemonCard(id) {
-    currentPokemonData = await getPokemonDetails(id);
-
-    let height = currentPokemonData.height;
-    let weight = currentPokemonData.weight;
-    let baseExperience = currentPokemonData.base_experience;
-    let abilities = currentPokemonData.abilities;
-
-    let abilitiesHTML ="";
-    for (let index = 0; index < abilities.length; index++) {
-        const ability = abilities[index].ability.name;
-        abilitiesHTML += `<span>${ability} </span>`
-    }
-  
-    for (let i = 0; i < pokemon.length; i++) {
-        if (getIdFromURL(pokemon[i]) == id) {
-        currentPokemonIndex = i;
-        break;
-        }    
-    }
+function renderPokemonOverlayCard(id, selectedPokemon, currentPokemonData, types, height, weight, baseExperience) {
+    let abilitiesHTML = buildAbilitiesHTML(currentPokemonData.abilities);
     
-    let selectedPokemon = pokemon[currentPokemonIndex];
-    let types = await getPokemonTypes(id);
-
-    let overlay = document.getElementById('overlay');
+    let overlayMain = document.getElementById('overlayMain');
     let overlayCard = document.getElementById('overlay-card');
+    overlayMain.classList.remove('d_none');
 
-    overlay.classList.remove('d_none');
     overlayCard.innerHTML = getOverlayCard(id, selectedPokemon.name, types, height, weight, baseExperience, abilitiesHTML);
 }
+
+
+function getOverlayCard(id, name, types, height, weight, baseExperience, abilitiesHTML) {
+    let typesHTML = "";
+
+    for (let i = 0; i < types.length; i++) {
+        typesHTML += `<span class="pokemon-type overlay-pokemon-type ${types[i]}">${types[i]}</span>`;
+    } return openOverlayCard(id, name, typesHTML, height, weight, baseExperience, abilitiesHTML);
+}
+
+
+function closePokemonCard() {
+    document.getElementById('overlayMain').classList.add('d_none');
+    document.getElementById('overlay-card').innerHTML = "";
+}
+
+
+async function openNextPokemon() {
+    currentPokemonIndex ++;
+
+    if (currentPokemonIndex >= pokemon.length) {
+        currentPokemonIndex = 0;
+    }
+
+    selectedPokemon = pokemon[currentPokemonIndex];
+    currentPokemonId = getIdFromURL(selectedPokemon);
+
+    currentPokemonData = await getPokemonDetails(currentPokemonId);
+
+    updatePokemonOverlay();
+    await getTypesPokemonOverlay();
+}
+
+
+async function openPreviousPokemon() {
+    currentPokemonIndex --;
+
+    if (currentPokemonIndex < 0) {
+        currentPokemonIndex = pokemon.length -1;
+    }
+
+    selectedPokemon = pokemon[currentPokemonIndex];
+    currentPokemonId = getIdFromURL(selectedPokemon);
+
+    currentPokemonData = await getPokemonDetails(currentPokemonId);
+
+    updatePokemonOverlay();
+    await getTypesPokemonOverlay();
+}
+
+
+async function getTypesPokemonOverlay() {
+    let types = await getPokemonTypes(currentPokemonId);
+
+    let typesHTML = "";
+    for (let i = 0; i < types.length; i++) {
+        typesHTML += `<span class="pokemon-type overlay-pokemon-type ${types[i]}">${types[i]}</span>`;
+        }
+    document.getElementById('overlayPokemonTypes').innerHTML = typesHTML;
+}
+
+
+function updatePokemonOverlay() {
+    document.getElementById('overlayPokemonId').innerText = `#${currentPokemonId}`;
+    document.getElementById('overlayPokemonName').innerText = selectedPokemon.name;
+    document.getElementById('overlayPokemonImage').src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${currentPokemonId}.png`
+    document.getElementById('overlayHeight').innerText = `${currentPokemonData.height} m`;
+    document.getElementById('overlayWeight').innerText = `${currentPokemonData.weight} kg`;
+    document.getElementById('overlayBaseExp').innerText = `${currentPokemonData.base_experience}`;
+    document.getElementById('overlayAbilities').innerHTML = buildAbilitiesHTML(currentPokemonData.abilities);
+}
+
 
 
